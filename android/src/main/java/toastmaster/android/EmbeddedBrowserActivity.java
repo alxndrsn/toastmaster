f68069edf6e5f6081dbaf7574fe9efc981f6ba85
Alex Anderson <anderson@medicmobile.org>
2015-06-12 10:34:09 +0200

    Add browser to android bridge for toasting.

--- android/src/main/java/toastmaster/android/EmbeddedBrowserActivity.java
+++ android/src/main/java/toastmaster/android/EmbeddedBrowserActivity.java
@@ -2,6 +2,7 @@ package toastmaster.android;
 
 import android.app.*;
 import android.webkit.*;
+import android.widget.*;
 import android.os.*;
 
 public class EmbeddedBrowserActivity extends Activity {
@@ -13,6 +14,9 @@ public class EmbeddedBrowserActivity extends Activity {
 
 		WebView container = (WebView) findViewById(R.id.WebViewContainer);
 		container.getSettings().setJavaScriptEnabled(true);
+		container.addJavascriptInterface(
+				new JsToaster(this),
+				"androidToaster");
 
 		final String url = "http://10.0.2.2:8080";
 		if(DEBUG) log("Pointing browser to %s", url);
@@ -22,5 +26,15 @@ public class EmbeddedBrowserActivity extends Activity {
 	private void log(String message, Object...extras) {
 		if(DEBUG) System.err.println(String.format(
 				message, extras));
+	}
+}
+
+class JsToaster {
+	private final Activity context;
+
+	public JsToaster(Activity context) { this.context = context; }
+
+	public void toast(String toast) {
+		Toast.makeText(context, toast, Toast.LENGTH_SHORT).show();
 	}
 }
